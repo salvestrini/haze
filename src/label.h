cf46ff3da93a1e69b6b986c48445a7281eb26a98
Francesco Salvestrini <salvestrini@gmail.com>
2009-10-05 21:58:39 +0200

    Added Label class placeholder

---
+++ src/label.h
@@ -0,0 +1,46 @@
+// -*- c++ -*-
+
+//
+// Copyright (C) 2009 Francesco Salvestrini
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#ifndef HAZE_LABEL_H
+#define HAZE_LABEL_H
+
+#include <string>
+
+#include "rectangle.h"
+
+namespace HAZE {
+
+        class Label : public Rectangle {
+        public:
+                Label(const std::string text);
+
+                const std::string & text() const { return text_; }
+
+                void text(const std::string & t) { text_ = t; }
+
+        protected:
+                std::string text_;
+
+        private:
+        };
+
+}
+
+#endif // HAZE_LABEL_H
