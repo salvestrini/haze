2aa4919a68f84392944d2b4fbce46050d8179f27
Francesco Salvestrini <salvestrini@gmail.com>
2010-12-20 00:52:25 +0100

    Added drawing primitives

---
+++ src/gfx/primitive.cxx
@@ -0,0 +1,147 @@
+//
+// Copyright (C) 2010 Francesco Salvestrini
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#include <cmath>
+
+#include "gfx/primitive.hxx"
+
+namespace HAZE {
+
+        namespace GL {
+
+                Color::Color(GLfloat red,
+                             GLfloat green,
+                             GLfloat blue,
+                             GLfloat alpha) :
+                        red_(red),
+                        green_(green),
+                        blue_(blue),
+                        alpha_(alpha)
+                { }
+
+                GLfloat Color::red()
+                { return red_; }
+
+                GLfloat Color::green()
+                { return green_; }
+
+                GLfloat Color::blue()
+                { return blue_; }
+
+                GLfloat Color::alpha()
+                { return alpha_; }
+
+                void Color::set()
+                { glColor4f(red_, green_, blue_, alpha_); }
+
+                Point::Point(const Color & color) :
+                        Color(color)
+                { }
+
+                Point::~Point()
+                { }
+
+                void Point::draw(const HAZE::Point<GLfloat> & where)
+                {
+                        set();
+
+                        glBegin(GL_LINES);
+                        glVertex2f(where.x(), where.y());
+                        glEnd();
+                }
+
+                Line::Line(const Color & color) :
+                        Color(color)
+                { }
+
+                Line::~Line()
+                { }
+
+                void Line::draw(const HAZE::Point<GLfloat> & from,
+                                const HAZE::Point<GLfloat> & to)
+                {
+                        set();
+
+                        glBegin(GL_LINES);
+                        glVertex2f(from.x(), from.y());
+                        glVertex2f(to.x(),   to.y());
+                        glEnd();
+                }
+
+                Rectangle::Rectangle(const Color & color,
+                                     bool          filled) :
+                        Color(color),
+                        filled_(filled)
+                { }
+
+                Rectangle::~Rectangle()
+                { }
+
+                void Rectangle::draw(const HAZE::Point<GLfloat> & from,
+                                     const HAZE::Point<GLfloat> & to)
+                {
+                        set();
+
+                        if (filled_) {
+                                glBegin(GL_QUADS);
+                        } else {
+                                glBegin(GL_LINE_LOOP);
+                        }
+
+                        glVertex2f(from.x(), from.y());
+                        glVertex2f(to.x(),   from.y());
+                        glVertex2f(to.x(),   to.y());
+                        glVertex2f(from.x(), to.y());
+                        glEnd();
+                }
+
+                Circle::Circle(const Color & color,
+                               GLfloat       radius,
+                               size_t        slices,
+                               bool          filled) :
+                        Color(color),
+                        radius_(radius),
+                        slices_(slices),
+                        filled_(filled)
+                { }
+
+                Circle::~Circle()
+                { }
+
+                void Circle::draw(const HAZE::Point<GLfloat> & center)
+                {
+                        set();
+
+                        if (filled_) {
+                                glBegin(GL_TRIANGLE_FAN);
+                        } else {
+                                glBegin(GL_LINE_LOOP);
+                        }
+
+                        glVertex2f(center.x(), center.y());
+
+                        for (size_t angle = 0; angle < 360; angle += 360 / slices_) {
+                                glVertex2f(center.x() + sin(angle) * radius_,
+                                           center.y() + cos(angle) * radius_);
+                        }
+
+                        glEnd();
+                }
+
+        }
+}
---
+++ src/gfx/primitive.hxx
@@ -0,0 +1,101 @@
+//
+// Copyright (C) 2010 Francesco Salvestrini
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#ifndef HAZE_GFX_PRIMITIVE
+#define HAZE_GFX_PRIMITIVE
+
+#include <GL/gl.h>
+
+#include "gfx/geometry.hxx"
+
+namespace HAZE {
+
+        namespace GL {
+
+                class Color {
+                public:
+                        Color(GLfloat red   = 1.0f,
+                              GLfloat green = 1.0f,
+                              GLfloat blue  = 1.0f,
+                              GLfloat alpha = 1.0f);
+                        ~Color();
+
+                        GLfloat red();
+                        GLfloat green();
+                        GLfloat blue();
+                        GLfloat alpha();
+
+                        void    set();
+
+                private:
+                        GLfloat red_;
+                        GLfloat green_;
+                        GLfloat blue_;
+                        GLfloat alpha_;
+                };
+
+                class Point : public Color {
+                public:
+                        Point(const Color & color);
+                        ~Point();
+
+                        void draw(const HAZE::Point<GLfloat> & where);
+                };
+
+                class Line : public Color {
+                public:
+                        Line(const Color & color);
+                        ~Line();
+
+                        void draw(const HAZE::Point<GLfloat> & from,
+                                  const HAZE::Point<GLfloat> & to);
+                };
+
+                class Rectangle : public Color {
+                public:
+                        Rectangle(const Color & color,
+                                  bool          filled = false);
+                        ~Rectangle();
+
+                        void draw(const HAZE::Point<GLfloat> & from,
+                                  const HAZE::Point<GLfloat> & to);
+
+                private:
+                        bool filled_;
+                };
+
+                class Circle : public Color {
+                public:
+                        Circle(const Color & color,
+                               GLfloat       radius,
+                               size_t        slices = 8,
+                               bool          filled = false);
+                        ~Circle();
+
+                        void draw(const HAZE::Point<GLfloat> & center);
+
+                private:
+                        GLfloat radius_;
+                        size_t  slices_;
+                        bool    filled_;
+                };
+
+        }
+}
+
+#endif
