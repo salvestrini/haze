1813012de8be34ba1618cedd7eae995ed8c29c12
Francesco Salvestrini <salvestrini@gmail.com>
2014-01-04 17:06:59 +0100

    Added fs and filesystem placeholders

---
+++ haze/fs/filesystem.cc
@@ -0,0 +1,110 @@
+//
+// Copyright (C) 2012, 2013, 2014 Francesco Salvestrini
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#include <string.h>
+#include <errno.h>
+
+#include <sys/types.h>
+
+// For opendir()/readdir()/closedir()
+#include <dirent.h>
+
+// For stat()
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include "haze/utils/string.hh"
+#include "haze/utils/log.hh"
+#include "haze/fs/filesystem.hh"
+
+namespace haze {
+        namespace fs {
+
+#define PATH_SEPARATOR "/"
+
+                path path::operator+(const path & rhs)
+                { return path(path_ + PATH_SEPARATOR + rhs.path_); }
+
+                bool path::exists() const
+                { return false; }
+
+                bool path::is_file() const
+                {
+                        struct stat buf;
+
+                        if (stat(path_.c_str(), &buf) != 0)
+                                throw cannot_stat("Cannot stat path "         +
+                                                  quote(path_)                +
+                                                  " (" + strerror(errno) + ")");
+
+                        if (S_ISREG(buf.st_mode)) return true;
+                        return false;
+                }
+
+                bool path::is_directory() const
+                {
+                        struct stat buf;
+
+                        if (stat(path_.c_str(), &buf) != 0)
+                                throw cannot_stat("Cannot stat path "         +
+                                                  quote(path_)                +
+                                                  " (" + strerror(errno) + ")");
+
+                        if (S_ISDIR(buf.st_mode)) return true;
+                        return false;
+                }
+
+                std::set<path *> directory::entries() const
+                {
+                        DIR * dir = opendir(str().c_str());
+                        if (dir == 0)
+                                throw cannot_walk(strerror(errno));
+
+                        std::set<path *> tmp;
+
+                        // Ugly use of dirent follows ...
+                        struct dirent * d;
+                        while ((d = readdir(dir)) != 0) {
+                                std::string name(d->d_name);
+
+                                if (d->d_type == DT_DIR) {
+                                        if ((name == ".") || (name == ".."))
+                                                continue;
+                                        tmp.insert(new directory(name));
+                                } else if (d->d_type == DT_REG) {
+                                        tmp.insert(new file(name));
+                                } else {
+                                        closedir(dir);
+                                        throw cannot_walk("Unhandled type "
+                                                          "for path " +
+                                                          quote(name));
+                                }
+                        }
+
+                        closedir(dir);
+                        return tmp;
+                }
+
+                void init()
+                { }
+
+                void fini()
+                { }
+
+        }
+}
---
+++ haze/fs/filesystem.hh
@@ -0,0 +1,100 @@
+//
+// Copyright (C) 2012, 2013, 2014 Francesco Salvestrini
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#ifndef HAZE_FS_FILESYSTEM
+#define HAZE_FS_FILESYSTEM
+
+#include <string>
+#include <set>
+
+#include "haze/core/exception.hh"
+
+namespace haze {
+        namespace fs {
+
+                class path {
+                public:
+                        path(const std::string & p) : path_(p) { }
+                        virtual ~path() { }
+
+                        class cannot_stat : public exception {
+                        public:
+                                cannot_stat(const std::string & what) :
+                                        exception(what) { }
+                                cannot_stat(const char * what) :
+                                        exception(what) { }
+                        };
+
+                        virtual bool is_file() const;
+                        virtual bool is_directory() const;
+                        virtual bool exists() const;
+
+                        virtual std::string str() const
+                        { return path_; }
+
+                        path operator+(const path & rhs);
+
+                private:
+                        std::string path_;
+                };
+
+                class file : public path {
+                public:
+                        file(const std::string & p) : path(p) { }
+                        virtual ~file() { }
+
+                        virtual bool is_file() const
+                        { return true; }
+
+                        virtual bool is_directory() const
+                        { return false; }
+
+                private:
+                };
+
+                class directory : public path {
+                public:
+                        directory(const std::string & p) : path(p) { }
+                        virtual ~directory() { }
+
+                        class cannot_walk : public exception {
+                        public:
+                                cannot_walk(const std::string & what) :
+                                        exception(what) { }
+                                cannot_walk(const char * what) :
+                                        exception(what) { }
+                        };
+
+                        std::set<path *> entries() const;
+
+                        virtual bool is_file() const
+                        { return false; }
+
+                        virtual bool is_directory() const
+                        { return true; }
+
+                private:
+                };
+
+                void init();
+                void fini();
+
+        }
+}
+
+#endif
